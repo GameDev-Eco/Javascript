644a65858fb394d06692ff75930629efe008c76f
GameDev-Eco <87069993+eco-gdv@users.noreply.github.com>
2023-01-07 11:19:29 -0300

    atualizado ex011

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,6 +1,8 @@
-var idade = 2
-if (idade > 18) {
-    console.log('Maior de idade!')
+var idade = 190
+if (idade < 16) {
+    console.log('Não vota!')
+}else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional!')
 }else{
-    console.log('Menor de idade')
+    console.log('Voto obrigatório!')
 }
